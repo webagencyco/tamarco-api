f2be23431a00e5534982418ec181f291af08920a
webagencyco <codewizardsuk@gmail.com>
2024-08-08 13:22:57 -0700

    adress and password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { register, login, changePassword, updateUserAddress } from '../controllers/authController.js';
 import { validateRegistration } from '../middleware/validate.js';
+import { authenticate } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
 router.post('/register', validateRegistration, register);
 router.post('/login', login);
-router.post('/change-password', changePassword);
-router.put('/update-address', updateUserAddress);
+router.post('/change-password',authenticate, changePassword);
+router.put('/update-address',authenticate, updateUserAddress);
 
 export default router;
